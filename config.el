5aa18c2f553e6b96b9d887bebc793452215b6cae
FLDPMpang <frozenpang@gmail.com>
2021-11-05 02:13:16 +0800

    format

--- config.el
+++ config.el
@@ -33,102 +33,104 @@
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
+
+(setq fcitx-remote-command "fcitx5-remote")
+
+;; (add-to-list 'initial-frame-alist 'fullboth)
 (add-hook 'window-setup-hook #'toggle-frame-maximized)
+;; (remove-hook 'doom-first-buffer-hook #'smartparens-global-mode)
 
-(defun colemak-config ()
+
 ;; colemak configured
+(defun colemak-config ()
+  ;; undo
+  (map! :m "l" nil
+        :n "l" 'evil-undo)
+  ;; insert
+  (map!   :m "k" nil
+          :m "K" nil
+          :n "k" 'evil-insert
+          :n "K" 'evil-insert-line
+          :v "k" evil-inner-text-objects-map
+          :v "K" 'evil-insert)
+  ;; unei for hjkl
+  (map!   :m "n" nil
+          :m "e" nil
+          :nv "u" nil
+          :nv "i" nil
+          :m "N" nil
+          :m "E" nil
+          :nv "U" nil
+          :nv "I" nil
+          :m "e" 'evil-next-visual-line
+          :m "u" 'evil-previous-visual-line
+          :m "n" 'evil-backward-char
+          :m "i" 'evil-forward-char
+          :m "I" 'evil-end-of-line
+          :m "N" 'evil-beginning-of-line
+          :v "e" 'evil-next-visual-line
+          :v "u" 'evil-previous-visual-line
+          :v "n" 'evil-backward-char
+          :v "i" 'evil-forward-char)
+  ;; searsh and jump
+  (map!   :m "h" nil
+          :m "j" nil
+          :m "H" nil
+          :m "J" nil
+          :m "h" 'evil-search-next
+          :m "H" 'evil-search-previous
+          :m "j" 'evil-forward-word-end
+          :m "J" 'evil-forward-WORD-end)
 
-;; undo
-(map! :m "l" nil
-      :n "l" 'evil-undo)
-
-;; insert 
-(map!   :m "k" nil
-        :m "K" nil
-        :n "k" 'evil-insert
-        :n "K" 'evil-insert-line
-        :v "k" evil-inner-text-objects-map
-        :v "K" 'evil-insert)
-
-;; unei for hjkl
-(map!   :m "n" nil
-        :m "e" nil
-        :nv "u" nil
-        :nv "i" nil
-        :m "N" nil
-        :m "E" nil
-        :nv "U" nil
-        :nv "I" nil
-        :m "e" 'evil-next-visual-line
-      :m "u" 'evil-previous-visual-line
-      :m "n" 'evil-backward-char
-      :m "i" 'evil-forward-char
-      :m "I" 'evil-end-of-line
-      :m "N" 'evil-beginning-of-line
-      :v "e" 'evil-next-visual-line
-      :v "u" 'evil-previous-visual-line
-      :v "n" 'evil-backward-char
-      :v "i" 'evil-forward-char)
-
-;; searsh and jump
-(map!   :m "h" nil
-        :m "j" nil
-        :m "H" nil
-        :m "J" nil
-        :m "h" 'evil-search-next
-        :m "H" 'evil-search-previous
-        :m "j" 'evil-forward-word-end
-        :m "J" 'evil-forward-WORD-end)
-
-;; window manage
-(define-key evil-window-map "b" 'evil-window-bottom-right)
-(define-key evil-window-map "c" 'evil-window-delete)
-(define-key evil-window-map "n" 'evil-window-left);;h
-(define-key evil-window-map "N" 'evil-window-move-far-left)
-(define-key evil-window-map "e" 'evil-window-down);;j
-(define-key evil-window-map "E" 'evil-window-move-very-bottom)
-(define-key evil-window-map "u" 'evil-window-up);;k
-(define-key evil-window-map "U" 'evil-window-move-very-top)
-(define-key evil-window-map "i" 'evil-window-right);;l
-(define-key evil-window-map "I" 'evil-window-move-far-right)
-(define-key evil-window-map "h" 'evil-window-new)
-(define-key evil-window-map "o" 'delete-other-windows)
-(define-key evil-window-map "p" 'evil-window-mru)
-(define-key evil-window-map "q" 'evil-quit)
-(define-key evil-window-map "r" 'evil-window-rotate-downwards)
-(define-key evil-window-map "R" 'evil-window-rotate-upwards)
-(define-key evil-window-map "s" 'evil-window-split)
-(define-key evil-window-map "S" 'evil-window-split)
-(define-key evil-window-map "t" 'evil-window-top-left)
-(define-key evil-window-map "v" 'evil-window-vsplit)
-(define-key evil-window-map "w" 'evil-window-next)
-(define-key evil-window-map "W" 'evil-window-prev)
-(define-key evil-window-map "+" 'evil-window-increase-height)
-(define-key evil-window-map "-" 'evil-window-decrease-height)
-(define-key evil-window-map "_" 'evil-window-set-height)
-(define-key evil-window-map "<" 'evil-window-decrease-width)
-(define-key evil-window-map ">" 'evil-window-increase-width)
-(define-key evil-window-map "=" 'balance-windows)
-(define-key evil-window-map "|" 'evil-window-set-width)
-(define-key evil-window-map "\C-b" 'evil-window-bottom-right)
-(define-key evil-window-map "\C-c" 'evil-window-delete)
-(define-key evil-window-map (kbd "C-S-h") 'evil-window-move-far-left)
-(define-key evil-window-map (kbd "C-S-j") 'evil-window-move-very-bottom)
-(define-key evil-window-map (kbd "C-S-k") 'evil-window-move-very-top)
-(define-key evil-window-map (kbd "C-S-l") 'evil-window-move-far-right)
-(define-key evil-window-map "\C-n" 'evil-window-new)
-(define-key evil-window-map "\C-o" 'delete-other-windows)
-(define-key evil-window-map "\C-p" 'evil-window-mru)
-(define-key evil-window-map "\C-r" 'evil-window-rotate-downwards)
-(define-key evil-window-map (kbd "C-S-r") 'evil-window-rotate-upwards)
-(define-key evil-window-map "\C-s" 'evil-window-split)
-(define-key evil-window-map (kbd "C-S-s") 'evil-window-split)
-(define-key evil-window-map "\C-t" 'evil-window-top-left)
-(define-key evil-window-map "\C-v" 'evil-window-vsplit)
-(define-key evil-window-map "\C-w" 'evil-window-next)
-(define-key evil-window-map (kbd "C-S-W") 'evil-window-prev)
-(define-key evil-window-map "\C-_" 'evil-window-set-height)
-(define-key evil-window-map "\C-f" 'ffap-other-window))
+  ;; window manage
+  (define-key evil-window-map "b" 'evil-window-bottom-right)
+  (define-key evil-window-map "c" 'evil-window-delete)
+  (define-key evil-window-map "n" 'evil-window-left);;h
+  (define-key evil-window-map "N" 'evil-window-move-far-left)
+  (define-key evil-window-map "e" 'evil-window-down);;j
+  (define-key evil-window-map "E" 'evil-window-move-very-bottom)
+  (define-key evil-window-map "u" 'evil-window-up);;k
+  (define-key evil-window-map "U" 'evil-window-move-very-top)
+  (define-key evil-window-map "i" 'evil-window-right);;l
+  (define-key evil-window-map "I" 'evil-window-move-far-right)
+  (define-key evil-window-map "h" 'evil-window-new)
+  (define-key evil-window-map "o" 'delete-other-windows)
+  (define-key evil-window-map "p" 'evil-window-mru)
+  (define-key evil-window-map "q" 'evil-quit)
+  (define-key evil-window-map "r" 'evil-window-rotate-downwards)
+  (define-key evil-window-map "R" 'evil-window-rotate-upwards)
+  (define-key evil-window-map "s" 'evil-window-split)
+  (define-key evil-window-map "S" 'evil-window-split)
+  (define-key evil-window-map "t" 'evil-window-top-left)
+  (define-key evil-window-map "v" 'evil-window-vsplit)
+  (define-key evil-window-map "w" 'evil-window-next)
+  (define-key evil-window-map "W" 'evil-window-prev)
+  (define-key evil-window-map "+" 'evil-window-increase-height)
+  (define-key evil-window-map "-" 'evil-window-decrease-height)
+  (define-key evil-window-map "_" 'evil-window-set-height)
+  (define-key evil-window-map "<" 'evil-window-decrease-width)
+  (define-key evil-window-map ">" 'evil-window-increase-width)
+  (define-key evil-window-map "=" 'balance-windows)
+  (define-key evil-window-map "|" 'evil-window-set-width)
+  (define-key evil-window-map "\C-b" 'evil-window-bottom-right)
+  (define-key evil-window-map "\C-c" 'evil-window-delete)
+  (define-key evil-window-map (kbd "C-S-h") 'evil-window-move-far-left)
+  (define-key evil-window-map (kbd "C-S-j") 'evil-window-move-very-bottom)
+  (define-key evil-window-map (kbd "C-S-k") 'evil-window-move-very-top)
+  (define-key evil-window-map (kbd "C-S-l") 'evil-window-move-far-right)
+  (define-key evil-window-map "\C-n" 'evil-window-new)
+  (define-key evil-window-map "\C-o" 'delete-other-windows)
+  (define-key evil-window-map "\C-p" 'evil-window-mru)
+  (define-key evil-window-map "\C-r" 'evil-window-rotate-downwards)
+  (define-key evil-window-map (kbd "C-S-r") 'evil-window-rotate-upwards)
+  (define-key evil-window-map "\C-s" 'evil-window-split)
+  (define-key evil-window-map (kbd "C-S-s") 'evil-window-split)
+  (define-key evil-window-map "\C-t" 'evil-window-top-left)
+  (define-key evil-window-map "\C-v" 'evil-window-vsplit)
+  (define-key evil-window-map "\C-w" 'evil-window-next)
+  (define-key evil-window-map (kbd "C-S-W") 'evil-window-prev)
+  (define-key evil-window-map "\C-_" 'evil-window-set-height)
+  (define-key evil-window-map "\C-f" 'ffap-other-window))
 (colemak-config)
 
 
--- init.el
+++ init.el
@@ -43,7 +43,7 @@
        ophints           ; highlight the region an operation acts on
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
        tabs              ; a tab bar for Emacs
-       ;;treemacs          ; a project drawer, like neotree but cooler
+       treemacs          ; a project drawer, like neotree but cooler
        unicode           ; extended unicode support for various languages
        vc-gutter         ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
@@ -99,7 +99,7 @@
        ;;pass              ; password manager for nerds
        ;;pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
-       ;;rgb               ; creating color strings
+       rgb               ; creating color strings
        ;;taskrunner        ; taskrunner for all your projects
        ;;terraform         ; infrastructure as code
        ;;tmux              ; an API for interacting with tmux
@@ -118,7 +118,7 @@
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
        ;;csharp            ; unity, .NET, and mono shenanigans
-       ;;data              ; config/data formats
+       data              ; config/data formats
        ;;(dart +flutter)   ; paint ui and not much else
        ;;dhall
        ;;elixir            ; erlang done right
@@ -148,7 +148,7 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       org               ; organize your plain life in plain text
+       (org +brain +dragndrop +gnuplot +jupyter)               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
